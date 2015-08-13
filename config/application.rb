292f5c7089f415addfa171eda1fa09fe5e1b13eb
mattdvhope <mattagape@me.com>
2015-08-13 12:30:25 +0700

    c

--- config/application.rb
+++ config/application.rb
@@ -26,6 +26,8 @@ module BackboneRails
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
+    config.assets.precompile + %w(*.png *.jpg *.jpeg *.gif *.woff *.woff2 *.ttf *.svg *.eot)
+
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
   end
