8f325787f4105e55261e8799055be5c12758893c
mattdvhope <mattagape@me.com>
2015-08-13 11:22:04 +0700

    c

--- app/assets/javascripts/sample.js
+++ app/assets/javascripts/sample.js
@@ -1,4 +1,4 @@
-var ready; // Done this way to deal with Turbolinks !!!
+var ready; // Done this way to deal with Turbolinks !!!  See... http://stackoverflow.com/questions/18770517/rails-4-how-to-use-document-ready-with-turbo-links
 ready = function() {
 
   console.log("Inside sample.js!!!!")
