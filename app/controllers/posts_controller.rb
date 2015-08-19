e90e9a428c6e36d36bf59ed7911b0d6c5c121039
mattdvhope <mattagape@me.com>
2015-08-19 16:46:44 +0700

    c

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,15 +1,13 @@
 class PostsController < ApplicationController
 
-  respond_to :json, :html
+  # respond_to :json, :html
 
   def index
     @posts = Post.all
-    respond_with @posts
   end
 
   def show
     @post = Post.find(params[:id])
-    respond_with @post
   end
 
   def create
