9a6eb3d559502e47d160ab5bc74fd3cf110279b2
mattdvhope <mattagape@me.com>
2015-09-04 06:02:06 +0700

    Changes

--- app/assets/javascripts/geolocation.js
+++ app/assets/javascripts/geolocation.js
@@ -13,11 +13,12 @@ var map = {
       height: this.height,
       src: this.buildURL()
     });
-    // document.body.innerHTML = $img.get(0); // this pure js is actually faster than jQuery below
-    $(document.body).html($img); // 
+    $("#map").html($img);
+  },
+  latLng: function() {
+    return "lat=" + this.position.coords.latitude + "&lon=" + this.position.coords.longitude;
   },
   build: function(position) { // build an image with this
-    console.log(this);
     this.position = position;
     this.buildImage();
     weather.get();
@@ -26,16 +27,41 @@ var map = {
 
 var weather = {
   endpoint: "http://api.openweathermap.org/data/2.5/weather",
+  template: Handlebars.compile($("#weather_card").html()),
+  $el: $("#weather"),
+  url: function() {
+    return this.endpoint + "?" + map.latLng();
+  },
   get: function() { // this is the  ajax call
     var dfd = $.ajax({ // the ajax method returns a "deferred" object, thus 'dfd' (common in jQuery usage)
-      url: this.endpoint + "?lat=" + map.position.coords.latitude + "&lon=" + map.position.coords.longitude,
+      url: this.url(),
       dataType: "json"
     });
-    dfd.done(function(json) { console.log(json); });
+    dfd.done(this.render.bind(this)); // 'done' and other dfd methods can be found at...  https://api.jquery.com/category/deferred-object/
+  },
+  temp: function(kelvin) {
+    return kelvinToF(kelvin).toFixed(1) + "&deg;F"; // in Handlebars' HTML, use a "triple moustache" to render w/o escaping--to allow the 'degree' symbol from '&deg;'
+  },
+  processData: function(json) {
+    console.log(json.weather[0].id);
+    return {
+      temp: this.temp(json.main.temp),
+      icon: json.weather[0].id, // See http://websygen.github.io/owfont/#usage
+      description: json.weather[0].description,
+      location: json.name
+    };
+  },
+  render: function(json) {
+    this.$el.html(this.template(this.processData(json))).addClass("slide");
   }
 };
 
+function kelvinToF(temp) {
+  return 9 / 5 * (temp - 273.15) + 32;
+}
+
 navigator.geolocation.getCurrentPosition(map.build.bind(map)); // use 'bind' b/c we don't want the context to be the window object
+
 
 
 
