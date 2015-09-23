bac9c8b0eb4d729002f36e0a38d6988b602a3a4f
mattdvhope <mattagape@me.com>
2015-09-23 14:19:48 +0700

    Uncommented

--- spec/javascripts/jasmine_hello_world_spec.js
+++ spec/javascripts/jasmine_hello_world_spec.js
@@ -1,15 +1,15 @@
-// describe('Hello World', function() {
-//   var helloWorld = new HelloWorld();
+describe('Hello World', function() {
+  var helloWorld = new HelloWorld();
 
-//   it('says hello world with no name', function() {
-//     expect(helloWorld.hello('')).toEqual('Hello, world!');
-//   });
+  it('says hello world with no name', function() {
+    expect(helloWorld.hello('')).toEqual('Hello, world!');
+  });
 
-//   it('says hello to bob', function() {
-//     expect(helloWorld.hello('Bob')).toEqual('Hello, Bob!');
-//   });
+  it('says hello to bob', function() {
+    expect(helloWorld.hello('Bob')).toEqual('Hello, Bob!');
+  });
 
-//   it('says hello to sally', function() {
-//     expect(helloWorld.hello('Sally')).toEqual('Hello, Sally!');
-//   });
-// });
+  it('says hello to sally', function() {
+    expect(helloWorld.hello('Sally')).toEqual('Hello, Sally!');
+  });
+});
