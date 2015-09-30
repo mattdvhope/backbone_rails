377452c0ee774919f8d45e32e56ce4d383256bb2
mattdvhope <mattagape@me.com>
2015-09-30 12:32:05 +0700

    Accessed collection of songs correctly

--- app/assets/javascripts/backbone/views/song.js
+++ app/assets/javascripts/backbone/views/song.js
@@ -19,7 +19,8 @@ var SongsView = Backbone.View.extend({
   },
   render: function() {
     this.$el.html(this.template({
-      songs: this.collection.toJSON(),
+      // songs: this.collection.toJSON(),
+      songs: this.collection.models[0].toJSON().songs,
       album: this.album
     }));
     this.open(); // to fade the overlay in...
