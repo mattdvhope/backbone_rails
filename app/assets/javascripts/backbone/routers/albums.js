2122e1a2ba09eceadcf00cb2acca70509ef2e43d
mattdvhope <mattagape@me.com>
2015-10-08 15:01:23 +0700

    Added comment

--- app/assets/javascripts/backbone/routers/albums.js
+++ app/assets/javascripts/backbone/routers/albums.js
@@ -3,7 +3,7 @@ var Router = Backbone.Router.extend({
     "albums/:name": "getAlbum"
   },
   getAlbum: function(name) {
-    App.fetchSongs(name);
+    App.fetchSongs(name); // 'fetchSongs' method in 'app.js'
   },
   index: function() {
     if (!App.songs.$el.is(":animated")) { // ':animated' (a jQuery pseudo-selector) here refers to 'faded in' ('fadeIn' is currently in operation)
