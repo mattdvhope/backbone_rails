bec35f0fdaf6e9657ceef7722f2f1ec43f1dce91
mattdvhope <mattagape@me.com>
2015-10-09 15:04:20 +0700

    Moved index to albums actionpack

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   mount MagicLamp::Genie, at: "/magic_lamp" if defined?(MagicLamp)
 
-  root to: "pages#index" 
+  root to: "albums#index" 
 
   resources :albums, only: [:index, :show, :new, :create]
 
