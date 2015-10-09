e24d17bf3baad1346b6fbe41c711b37783ca7886
mattdvhope <mattagape@me.com>
2015-10-10 05:30:20 +0700

    Added comments

--- app/assets/javascripts/backbone/views/albums/index.js
+++ app/assets/javascripts/backbone/views/albums/index.js
@@ -1,6 +1,6 @@
-var AlbumsView = Backbone.View.extend({
+var AlbumsView = Backbone.View.extend({ // 'this' is a Backbone View constructor
   template:  HandlebarsTemplates['albums/index'],
-  render: function() {
+  render: function() { // 'this.$el' is '<ul id="albums">' in app/views/albums/index.html.erb
     this.$el.html(this.template({ albums: this.collection.toJSON() })); // 'albums' here is the 'albums' that we'll use in the Handlebars 'each' loop in the html page
   },  // $el is the current jQuery object that represents the view, in this case a <div> (default) at this moment
   initialize: function() {
