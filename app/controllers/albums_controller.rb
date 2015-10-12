ec4938490c3ca7a1938d698c8ccaa20a0268f2d1
mattdvhope <mattagape@me.com>
2015-10-12 22:04:41 +0700

    Worked on create action

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,5 +1,7 @@
 class AlbumsController < ApplicationController
 
+  protect_from_forgery except: :create
+
   def index
     @album = Album.new
     gon.album = @album
@@ -15,11 +17,26 @@ class AlbumsController < ApplicationController
   end
 
   def new
-    redirect_to root_path 
+    redirect_to root_path # in case of page-refresh
   end
 
   def create
-    
+    @album = Album.new
+binding.pry
+    @album.assign_attributes(params)
+    # @album.artist = params[:artist]
+    # @album.title = params[:title]
+    # @album.url = params[:url]
+    # @album.date = params[:date]
+    # @album.cover = params[:cover]
+    # @album.price = params[:price].to_i
+    # @album.save
   end
+
+  private
+
+    def album_params
+      params.require(:album).permit(:artist, :title, :url, :date, :cover, :price)
+    end
 
 end
