dcf4b624bd311a1a44d9d7ed228f2e635a87dd76
mattdvhope <mattagape@me.com>
2015-10-12 22:00:55 +0700

    Added addNewAlbum method

--- app/assets/javascripts/backbone/routers/albums.js
+++ app/assets/javascripts/backbone/routers/albums.js
@@ -1,22 +1,26 @@
 var Router = Backbone.Router.extend({
   routes: {
-    "albums/new": "newAlbum",
+    "albums/new": "addNewAlbum",
     "albums/:name": "getAlbum"
   },
   getAlbum: function(name) {
     App.fetchSongs(name); // 'fetchSongs' method in 'app.js'
   },
-  newAlbum: function() {
-    var newLp = new Album()
-    console.log(newLp);
+  addNewAlbum: function() {
+    // var newLp = gon.album
+    // console.log(newLp);
+    App.getAlbumForm();
   },
   index: function() {
-    if (!App.songs.$el.is(":animated")) { // ':animated' (a jQuery pseudo-selector) here refers to 'faded in' ('fadeIn' is currently in operation)
-      App.songs.fadeOut();
-    }
+    var modal = App.songs || App.new_form
+      if (!modal.$el.is(":animated")) { // ':animated' (a jQuery pseudo-selector) here refers to 'faded in' ('fadeIn' is currently in operation)
+        modal.fadeOut();
+      }
   },
   initialize: function() {
     this.route(/^\/?$/, "index", this.index); // listening for a path that starts with a '/' which will be our 'index' & we'll call the current 'index' method in 'Router'
   }
 });
+      
+
 
