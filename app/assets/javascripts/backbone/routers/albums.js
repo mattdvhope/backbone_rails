2a6671a306d297a907c5f8ad254cce33f1b29ad4
mattdvhope <mattagape@me.com>
2015-10-13 11:07:41 +0700

    Removed console log

--- app/assets/javascripts/backbone/routers/albums.js
+++ app/assets/javascripts/backbone/routers/albums.js
@@ -13,9 +13,9 @@ var Router = Backbone.Router.extend({
   },
   index: function() {
     var modal = App.songs || App.new_form
-      if (!modal.$el.is(":animated")) { // ':animated' (a jQuery pseudo-selector) here refers to 'faded in' ('fadeIn' is currently in operation)
-        modal.fadeOut();
-      }
+    if (!modal.$el.is(":animated")) { // ':animated' (a jQuery pseudo-selector) here refers to 'faded in' ('fadeIn' is currently in operation)
+      modal.fadeOut();
+    }
   },
   initialize: function() {
     this.route(/^\/?$/, "index", this.index); // listening for a path that starts with a '/' which will be our 'index' & we'll call the current 'index' method in 'Router'
