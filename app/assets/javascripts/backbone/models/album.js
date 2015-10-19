9d7ca8a3f0a03d5a5c783e08bd74237ae09c0175
mattdvhope <mattagape@me.com>
2015-10-19 16:11:51 +0700

    Added delete_album attr

--- app/assets/javascripts/backbone/models/album.js
+++ app/assets/javascripts/backbone/models/album.js
@@ -2,7 +2,7 @@ var Album = Backbone.Model.extend({
   parse: function(attrs) {
     attrs.songs_url = "/albums/" + attrs.title;
     attrs.edit_album = Routes.edit_album_path(attrs.id);
-    attrs.for_album_action = Routes.album_path(attrs.id);
+    attrs.delete_album = Routes.album_path(attrs.id);
     return attrs;
   }
 });
