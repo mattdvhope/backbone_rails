d5252ac3390662f778c04a71dc07dfcd29d50a0c
mattdvhope <mattagape@me.com>
2015-10-19 14:21:45 +0700

    Added more attributes to add more paths to rails controller actions

--- app/assets/javascripts/backbone/models/album.js
+++ app/assets/javascripts/backbone/models/album.js
@@ -1,7 +1,8 @@
 var Album = Backbone.Model.extend({
   parse: function(attrs) {
     attrs.songs_url = "/albums/" + attrs.title;
-    attrs.edit_album_path = Routes.edit_album_path(attrs.id);
+    attrs.edit_album = Routes.edit_album_path(attrs.id);
+    attrs.for_album_action = Routes.album_path(attrs.id);
     return attrs;
   }
 });
