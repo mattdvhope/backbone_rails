247ad4d301413f275583a979d9baa44fb751c2f0
mattdvhope <mattagape@me.com>
2015-10-19 13:43:02 +0700

    Added edit_album_path attribute to album model

--- app/assets/javascripts/backbone/models/album.js
+++ app/assets/javascripts/backbone/models/album.js
@@ -1,6 +1,7 @@
 var Album = Backbone.Model.extend({
   parse: function(attrs) {
     attrs.songs_url = "/albums/" + attrs.title;
+    attrs.edit_album_path = Routes.edit_album_path(attrs.id);
     return attrs;
   }
 });
