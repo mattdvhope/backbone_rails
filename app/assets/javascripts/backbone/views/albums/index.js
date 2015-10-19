d2af8245394c740869b1d1cf201cccbd735d38d9
mattdvhope <mattagape@me.com>
2015-10-19 13:42:20 +0700

    Edited template params

--- app/assets/javascripts/backbone/views/albums/index.js
+++ app/assets/javascripts/backbone/views/albums/index.js
@@ -1,7 +1,7 @@
 var AlbumsView = Backbone.View.extend({ // 'this' is a Backbone View constructor
   template:  HandlebarsTemplates['albums/index'],
   render: function() { // 'this.$el' is '<ul id="albums">' in app/views/albums/index.html.erb
-    this.$el.html(this.template({ albums: this.collection.toJSON(), edit_album_path: Routes.edit_album_path(1) })); // 'albums' here is the 'albums' that we'll use in the Handlebars 'each' loop in the html page
+    this.$el.html(this.template({ albums: this.collection.toJSON() })); // 'albums' here is the 'albums' that we'll use in the Handlebars 'each' loop in the html page
   },  // $el is the current jQuery object that represents the view, in this case a <div> (default) at this moment
   initialize: function() { // when 'new AlbumsView()' is instantiated, 'this.$el' is defined here...
     this.$el = $("#albums"); // overriding the '$el' default '<div>' here to the '<ul>' in 'app/views/albums/index.html.erb'
