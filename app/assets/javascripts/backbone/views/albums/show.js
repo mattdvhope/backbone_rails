0a9f45dd190fe7bd7bc293f77ef33aeb8d82af40
mattdvhope <mattagape@me.com>
2015-10-20 20:55:11 +0700

    Removed some methods

--- app/assets/javascripts/backbone/views/albums/show.js
+++ app/assets/javascripts/backbone/views/albums/show.js
@@ -2,7 +2,11 @@ var AlbumView = Backbone.View.extend({
   tagName: "li",
   template:  HandlebarsTemplates['albums/show'],
   render: function() {
+    var id = this.model.get("id")
+
+    this.$el.attr("id", "album_" + id);
     this.$el.html(this.template(this.model.toJSON()));
+    this.$el.appendTo(App.$el.find("ul"));
   },
   initialize: function() {
     this.render();
