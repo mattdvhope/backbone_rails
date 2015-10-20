16dd726e0e4173bc6b3c663802e9dc306411d803
mattdvhope <mattagape@me.com>
2015-10-20 20:51:01 +0700

    Added quantity

--- app/assets/javascripts/backbone/models/album.js
+++ app/assets/javascripts/backbone/models/album.js
@@ -1,5 +1,6 @@
 var Album = Backbone.Model.extend({
   parse: function(attrs) {
+    attrs.quantity = 0;
     attrs.songs_url = "/albums/" + attrs.title;
     attrs.edit_album = Routes.edit_album_path(attrs.id);
     attrs.delete_album = Routes.album_path(attrs.id);
