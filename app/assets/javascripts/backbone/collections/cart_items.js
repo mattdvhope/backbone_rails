8598db9c6d94d5509b2175de92cb230fbe7a2133
mattdvhope <mattagape@me.com>
2015-10-20 21:05:30 +0700

    Added console logs & set up some methods

---
+++ app/assets/javascripts/backbone/collections/cart_items.js
@@ -0,0 +1,25 @@
+var CartItems = Backbone.Collection.extend({
+  setTotal: function() {
+    this.total = this.toJSON().reduce(function(a, b) {
+      return a + b.price * b.quantity;
+    }, 0);
+  },
+  getTotal: function() { return this.total; },
+  setQuantity: function() {
+    this.quantity = this.toJSON().reduce(function(a, b) {
+      return a + b.quantity;
+    }, 0);
+  },
+  getQuantity: function() { return this.quantity; },
+  addItem: function(item) {
+    var item = item.clone();
+    item.set("quantity", 1);
+console.log(item.toJSON());
+    this.add(item);
+console.log(this.toJSON());
+    this.setTotal();
+console.log(this.total);
+    this.setQuantity();
+console.log(this.quantity);
+  }
+});
