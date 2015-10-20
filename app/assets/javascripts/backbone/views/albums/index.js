f04691931f420cea95b90972f7b1a0341bdb11cf
mattdvhope <mattagape@me.com>
2015-10-20 20:47:51 +0700

    Removed comments

--- app/assets/javascripts/backbone/views/albums/index.js
+++ app/assets/javascripts/backbone/views/albums/index.js
@@ -1,11 +1,11 @@
-var AlbumsView = Backbone.View.extend({ // 'this' is a Backbone View constructor
+var AlbumsView = Backbone.View.extend({
   template:  HandlebarsTemplates['albums/index'],
-  render: function() { // 'this.$el' is '<ul id="albums">' in app/views/albums/index.html.erb
-    this.$el.html(this.template({ albums: this.collection.toJSON() })); // 'albums' here is the 'albums' that we'll use in the Handlebars 'each' loop in the html page
-  },  // $el is the current jQuery object that represents the view, in this case a <div> (default) at this moment
-  initialize: function() { // when 'new AlbumsView()' is instantiated, 'this.$el' is defined here...
-    this.$el = $("#albums"); // overriding the '$el' default '<div>' here to the '<ul>' in 'app/views/albums/index.html.erb'
-    this.listenTo(this.collection, "change", this.render); // any change to the collection causes us to re-render the page
+  render: function() {
+    this.$el.html(this.template({ albums: this.collection.toJSON() }));
+  },
+  initialize: function() {
+    this.$el = $("#albums");
+    this.listenTo(this.collection, "change", this.render);
   }
 });
 
