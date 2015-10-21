a6a202857d247325c74ba551d875cc58907b8e28
mattdvhope <mattagape@me.com>
2015-10-22 05:57:34 +0700

    Edited addItem to account for multiple selections of that item

--- app/assets/javascripts/backbone/collections/cart_items.js
+++ app/assets/javascripts/backbone/collections/cart_items.js
@@ -1,25 +1,35 @@
 var CartItems = Backbone.Collection.extend({
-  setTotal: function() {
+  setTotalPrice: function() {
     this.total = this.toJSON().reduce(function(a, b) {
-      return a + b.price * b.quantity;
+      return a + +b.price * b.item_quantity;
     }, 0);
+
+    return this; // to allow for method-chaining below
   },
-  getTotal: function() { return this.total; },
-  setQuantity: function() {
+  getTotalPrice: function() { return this.total; },
+  setCartQuantity: function() {
     this.quantity = this.toJSON().reduce(function(a, b) {
-      return a + b.quantity;
+      return a + b.item_quantity;
     }, 0);
+
+    return this; // to allow for method-chaining below
   },
   getQuantity: function() { return this.quantity; },
   addItem: function(item) {
-    var item = item.clone();
-    item.set("quantity", 1);
-console.log(this.toJSON());
-    this.add(item);
-console.log(this.toJSON());
-    this.setTotal();
-// console.log(this.quantity);
-    this.setQuantity();
-// console.log(this.quantity);
+    var existing = this.get(item.get("id")); // to check if item is already in 'this' collection or not
+    if (existing) {
+      existing.set("item_quantity", existing.get("item_quantity") + 1);
+      existing.set("price_multiplied", existing.get("item_quantity") * +existing.get("price"))
+    } else {
+      existing = item.clone();
+      existing.set("item_quantity", 1);
+      existing.set("price_multiplied", +existing.get("price"))
+      this.add(existing);
+    }
+    this.setTotalPrice().setCartQuantity();
+    this.trigger("cart_updated");
   }
 });
+
+
+
