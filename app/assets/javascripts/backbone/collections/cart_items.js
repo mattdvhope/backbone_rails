a7e05f0d30021a591e538d843af5e1335d1df558
mattdvhope <mattagape@me.com>
2015-10-22 17:49:58 +0700

    Set up destroy and local storage

--- app/assets/javascripts/backbone/collections/cart_items.js
+++ app/assets/javascripts/backbone/collections/cart_items.js
@@ -15,6 +15,14 @@ var CartItems = Backbone.Collection.extend({
     return this; // to allow for method-chaining below
   },
   getQuantity: function() { return this.quantity; },
+  readStorage: function() {
+    stored_cart = JSON.parse(localStorage.getItem("cart")); // we need to take 'localStorage.getItem("cart")' from a string and turn it into a javascript object
+    this.reset(stored_cart); // we'll reset this collection with the 'stored_cart' data
+    this.setTotalPrice().setCartQuantity();
+  },
+  updateStorage: function() {
+    localStorage.setItem("cart", JSON.stringify(this.toJSON())); // we need to convert this collection into a string
+  },
   addItem: function(item) {
     var existing = this.get(item.get("id")); // to check if item is already in 'this' collection or not
     if (existing) {
@@ -26,8 +34,19 @@ var CartItems = Backbone.Collection.extend({
       existing.set("price_multiplied", +existing.get("price"))
       this.add(existing);
     }
-    this.setTotalPrice().setCartQuantity();
+    this.update();
     this.trigger("cart_updated");
+  },
+  destroy: function(id) {
+    this.remove(id);
+    this.update();
+  },
+  update: function() {
+    this.setTotalPrice().setCartQuantity().updateStorage();
+  },
+  initialize: function() {
+    this.readStorage()
+    this.on("destroy", this.destroy);
   }
 });
 
--- app/assets/javascripts/backbone/views/cart/index.js
+++ app/assets/javascripts/backbone/views/cart/index.js
@@ -1,15 +1,25 @@
 var CartView = Backbone.View.extend({
   template:  HandlebarsTemplates['cart/index'],
   el: $("#cart").get(0),
+  events: {
+    "click a.remove": "destroy"
+  },
+  destroy: function(e) {
+    e.preventDefault();
+    var $e = $(e.target);
+    this.collection.trigger("destroy", +$e.attr("data-id"));
+    this.render();
+  },
   render: function() {
     this.$el.html(this.template({
-      quantity: this.collection.getQuantity(), // from 'collections/cart_items.js'
+      quantity: this.collection.getQuantity(), // cart's total quantity
       items: this.collection.toJSON(),
-      total: this.collection.getTotal() // from 'collections/cart_items.js'
+      total: this.collection.getTotalPrice() // price-total of every item in cart
     }));
   },
   initialize: function() {
     this.render();
+    this.listenTo(this.collection, "cart_updated", this.render);
   }
 });
 
