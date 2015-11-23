d950fa6f186928f1042a4fd5aa67a6c99e22b69b
mattdvhope <mattagape@me.com>
2015-11-23 15:47:54 +0700

    Put in title rather than id for edit

--- app/assets/javascripts/backbone/models/album.js
+++ app/assets/javascripts/backbone/models/album.js
@@ -1,7 +1,8 @@
 var Album = Backbone.Model.extend({
   parse: function(attrs) {
     attrs.songs_url = "/albums/" + attrs.title;
-    attrs.edit_album = Routes.edit_album_path(attrs.id);
+    var album_title = {id: +attrs.id, title: attrs.title, to_param: attrs.title};
+    attrs.edit_album = Routes.edit_album_path(album_title); // => "/albums/With%20Footnotes/edit"
     attrs.delete_album = Routes.album_path(attrs.id);
     return attrs;
   }
