f6e96847c201aed08bdc17bda4d60a148a5064dd
mattdvhope <mattagape@me.com>
2016-03-22 09:22:01 +0700

    Set up show view for each category

--- app/assets/javascripts/backbone/views/categories/show.js
+++ app/assets/javascripts/backbone/views/categories/show.js
@@ -1,5 +1,35 @@
 window.CategoryView = Backbone.View.extend({
 
-  
+  handle_bars_template:HandlebarsTemplates['categories/show'],
+  events: {
+    'click .delete': 'removeCategory'
+  },
+  initialize: function() { // 'initialize' is the constructor of this particular view
+    _.bindAll(this, 'render', 'removeCategory');
+    this.collection.fetch();
+    this.collection.bind('all', this.render);
+  },
+
+  removeCategory: function(e) {
+    e.preventDefault();
+    var id = $(e.target).parents('li').data('id');
+    var model = this.collection.where({ id: id })[0];
+    console.log(model.toJSON());
+  },
+
+  render: function() {
+    $(this.el).html(this.handle_bars_template());
+    this.collection.each(function(cat) {
+      var li_template = HandlebarsTemplates['categories/single_category']
+
+      $(this.el).find('.categories').append(li_template({
+        id: cat.get('id'),
+        name: cat.get('name')
+      }));
+
+    }, this); // The 'this' at the end causes the 'this' inside of this 'each' statement to be the same 'this' that is part of the rest of the app.
+
+    return this;
+  }
 
 });
