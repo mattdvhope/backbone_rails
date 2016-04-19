11a3e2d3d926b06111a573c05936d981654b4b9d
mattdvhope <mattagape@me.com>
2016-04-19 14:19:37 +0700

    Edited show to handle deleting and adding categories in order

--- app/assets/javascripts/backbone/views/categories/show.js
+++ app/assets/javascripts/backbone/views/categories/show.js
@@ -1,6 +1,6 @@
 window.CategoryView = Backbone.View.extend({
 
-  className: "a-class",
+  // className: "a-class",
   show_template:HandlebarsTemplates['categories/show'],
   events: {
     'click .close': 'hide',
@@ -8,9 +8,9 @@ window.CategoryView = Backbone.View.extend({
     'click .category_add': 'addCategory'
   },
   initialize: function() { // 'initialize' is the constructor of this particular view
-    _.bindAll(this, 'render', 'removeCategory', 'addCategory');
+    _.bindAll(this, 'render');
     this.collection.fetch();
-    this.collection.bind('all', this.render);
+    this.collection.bind('sync', this.render);
   },
 
   toggle: function() {
@@ -20,6 +20,7 @@ window.CategoryView = Backbone.View.extend({
   addCategory: function(e) {
     e.preventDefault();
     this.collection.create({ name: $(this.el).find('.category_name').val() });
+    this.collection.fetch();
   },
 
   removeCategory: function(e) {
@@ -28,19 +29,29 @@ window.CategoryView = Backbone.View.extend({
     var model = this.collection.where({ id: id })[0];
     this.collection.remove(model);
     if (model) { model.destroy(); }
+    this.render();
   },
 
   render: function() {
     $(this.el).html(this.show_template());
 
-    this.collection.each(function(cat) {
-      var li_template = HandlebarsTemplates['categories/single_category']
+    this.collection.models = this.collection.models.sort(function(a, b) {
+        a = a.get('id');
+        b = b.get('id');
+        if(a > b)
+            return 1;
+        if(a < b)
+            return -1;
+        return 0;
+    });
 
+    this.collection.models.forEach(function(cat) {
+      var li_template = HandlebarsTemplates['categories/single_category']
       $(this.el).find('.categories').append(li_template({
-        id: cat.get('id'),
-        name: cat.get('name')
+        id: cat.toJSON().id,
+        name: cat.toJSON().name
       }));
-    }, this); // The 'this' at the end causes the 'this' inside of this 'each' statement to be the same 'this' that is part of the rest of the app.
+    }, this); // The 'this' at the end causes the 'this' inside of this 'forEach' statement to be the same 'this' that is part of the rest of the app.
 
     return this; // to chain methods to 'render()'
   }
